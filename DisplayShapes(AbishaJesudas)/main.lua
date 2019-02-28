352a48e595b316c7d1788fc0c26c47d8546b2821
Abisha Jesudas <47784371+s445791@users.noreply.github.com>
2019-02-28 09:00:43 -0500

    Initial commit

---
+++ DisplayShapes(AbishaJesudas)/main.lua
@@ -0,0 +1,17 @@
+ -- Title: AreaOfRectangleAndCircle
+-- Name: Abisha Jesudas
+-- Course: ICS2O/3C
+-- This program i show you how to make adisplay shapes on an ipad 
+
+local areaText
+local textSize = 50
+local myTriangle
+local verticesTriangle = { 0,-50, 50,50, -50,50 }
+
+-- set the background colour of my screen.
+display.setDefault("background", 255/255, 255/255, 255/255)
+
+-- draw a triangle that is the width and the height
+myTriangle = display.newPolygon(0,0 )
+
+
