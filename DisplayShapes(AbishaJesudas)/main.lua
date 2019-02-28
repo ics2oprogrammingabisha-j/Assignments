08a38c5558731285dd4c5aa09f9ed3df8de3ee9f
Abisha Jesudas <47784371+s445791@users.noreply.github.com>
2019-02-28 15:57:47 -0500

    Update main.lua

--- DisplayShapes(AbishaJesudas)/main.lua
+++ DisplayShapes(AbishaJesudas)/main.lua
@@ -9,9 +9,28 @@ local myTriangle
 local verticesTriangle = { 0,-50, 50,50, -50,50 }
 
 -- set the background colour of my screen.
-display.setDefault("background", 255/255, 255/255, 255/255)
+display.setDefault("background", 200/255, 255/255, 255/255)
 
 -- draw a triangle that is the width and the height
-myTriangle = display.newPolygon(0,0 )
+myTriangle = display.newPolygon(500, 500, verticesTriangle)
+
+-- anchor the rectangle in the top left corner of the screen and it sets its(x,y) position
+myTriangle.anchorX = 0
+myTriangle.anchorY = 0
+myTriangle.x = 440
+myTriangle.y = 320
+
+-- set the width of the border
+myTriangle. strokeWidth = 10
+
+-- set the color of the triangle
+myTriangle:setFillColor(255/255, 255/255, 255/255)
+
+-- set the color of the border
+myTriangle:setStrokeColor(0/255, 0/255, 0/255)
+
+-- calculate the area
+areaOfTriangle = baseOfTriangle * heigthOfTriangle
+
 
 
