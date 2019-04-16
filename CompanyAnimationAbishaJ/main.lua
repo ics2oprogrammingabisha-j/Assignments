3f62410b44c685239da039760ccc4f5b1616c5b8
Abisha Jesudas <47784371+s445791@users.noreply.github.com>
2019-04-16 15:20:14 -0400

    adde stuff

--- CompanyAnimationAbishaJ/main.lua
+++ CompanyAnimationAbishaJ/main.lua
@@ -8,38 +8,67 @@
 -- hide the status bar 
 display.setStatusBar(display.HiddenStatusBar)
 
--- global variables
-scrollspeed = 2
+---------------------------------------------------------------------------------------------------
+-- SOUNDS
+---------------------------------------------------------------------------------------------------
 
+--  sound
+local DogBarkingSound = audio.loadSound( "Sounds/DogBarking.mp3") -- setting a variable to an mp3 file
+local DogBarkingSoundChannel
+local RocketshipSound = audio.loadSound( "Sounds/Rocketship.mp3") -- setting a variable to an mp3 file
+local RocketshipSoundChannel
+
+-- play each sound
+DogBarkingSoundChannel = audio.play(DogBarkingSound)
+RocketshipSoundChannel = audio.play(RocketshipSound)
+
+---------------------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+---------------------------------------------------------------------------------------------------
 
 -- background image with width and height
-local CompLogo = display.newImageRect("Images/CompanyLogoAbishaJ@2x.png", 600, 600)
+local CompLogo 
+-- charector width and height
+local Rocketship 
+
+---------------------------------------------------------------------------------------------------
+-- OBJECT CREATION
+---------------------------------------------------------------------------------------------------
+CompLogo = display.newImageRect("Images/CompanyLogoAbishaJStars@2x.png", 600, 600)
 
 -- set the initial x and y postion of CompanyLogo
 CompLogo.x = 500
 CompLogo.y = display.contentHeight/2
 
+Rocketship = display.newImageRect("Images/CompanyLogoAbishaJRocketship@2x.png", 400, 200)
+
+-- set the image to be transparent
+Rocketship.alpha = 0
+
+-- set the initial x and y position of the r\Rocketship
+Rocketship.x = 1024
+Rocketship.y = 350
+
+transition.to(Rocketship, {x=525, y=350, time=1500})
+---------------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
 ---------------------------------------------------------------------------------------------------
 
--- global variables
-scrollSpeed = 5
-
--- charector width and height
-local rocketship = display.newImageRect("Images/CompanyLogoAbishaJRocketshipandDog@2x.psd", 200, 200)
-
--- set the initial x and y position of the rocketship
-rocketship.x = 1024
-rocketship.y = display.contentHeight/2
 
 -- function: Moveship
 -- Input: this function accepts an event listener
 -- Output none
 -- description : this function adds the scroll speed to the x-value of the ship
-local function Moveship(event)
-	-- add the scrool speed to the x-value of the ship
-	rocketship.x = rocketship.x - scrollSpeed
+local function FadeinRocketship(event)
 	-- change the transparency of the ship so that every time it moves it fades out.
-	rocketship.alpha = rocketship.alpha + 0.01
+	Rocketship.alpha = Rocketship.alpha + 0.01
+
 end
+
+---------------------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+---------------------------------------------------------------------------------------------------
+
 -- moveship will be called over and over again
-Runtime:addEventListener("enterFrame", Moveship)
+Runtime:addEventListener("enterFrame", FadeinRocketship)
+--------------------------------------------------------------------------------------
