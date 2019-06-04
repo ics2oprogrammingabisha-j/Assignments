d03171f98c58efd04a7bb5dba252790cbb6cfca8
Abisha Jesudas <47784371+s445791@users.noreply.github.com>
2019-06-04 09:08:14 -0400

    added stuff

--- CompanyAnimationAbishaJ/main.lua
+++ CompanyAnimationAbishaJ/main.lua
@@ -13,7 +13,7 @@ scrollspeed = 2
 
 
 -- background image with width and height
-local CompLogo = display.newImageRect("Images/CompanyLogoAbishaJ@2x.png", 600, 600)
+local CompLogo = display.newImageRect("Images/CompanyLogoAbishaJStars@2x.png", 600, 600)
 
 -- set the initial x and y postion of CompanyLogo
 CompLogo.x = 500
@@ -25,21 +25,21 @@ CompLogo.y = display.contentHeight/2
 scrollSpeed = 5
 
 -- charecter width and height
-local rocketship = display.newImageRect("Images/CompanyLogoAbishaJRocketshipandDog@2x.psd", 200, 200)
+local rocketship = display.newImageRect("Images/CompanyLogoAbishaJRocketship@2x.png", 400, 200)
 
 -- set the initial x and y position of the rocketship
 rocketship.x = 1024
 rocketship.y = display.contentHeight/2
+
+-- set the ending x and y position of the rocketship
+transition.to(rocketship, {x = 500, y = display.contentHeight/2})
 
 -- function: Moveship
 -- Input: this function accepts an event listener
 -- Output none
 -- description : this function adds the scroll speed to the x-value of the ship
 local function Moveship(event)
-	-- add the scrool speed to the x-value of the ship
-	rocketship.x = rocketship.x - scrollSpeed
-	-- change the transparency of the ship so that every time it moves it fades out.
-	rocketship.alpha = rocketship.alpha + 0.01
+	
 end
 -- moveship will be called over and over again
 Runtime:addEventListener("enterFrame", Moveship)
